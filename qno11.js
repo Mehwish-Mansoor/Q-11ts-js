2ce28b383763f72c8ec4fce8fbb9228ba721e7c5
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-13 11:26:10 +0500

    Add files via upload

---
+++ qno11.js
@@ -0,0 +1,11 @@
+"use strict";
+// 11. Name: store the name of few of yoyr frinds in a arry called name .Print each person's name by accessing each element in the list, one at a time. 
+let names = ["Mehwish", "Fatima", "Umm-e-Hani", "Mansoor", "Salman", "Farman", "Kashan", "Nabeel"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
+console.log(names[5]);
+console.log(names[6]);
+console.log(names[7]);
